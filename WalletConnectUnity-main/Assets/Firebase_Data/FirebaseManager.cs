4168a6bbe02ba4a7a8bee8bd00f6ac230e87102b
Karan <karan1ugd43wp@gmail.com>
2022-02-20 22:08:57 +0530

    Added Proper Saving and Retreiving Calls;

--- WalletConnectUnity-main/Assets/Firebase_Data/FirebaseManager.cs
+++ WalletConnectUnity-main/Assets/Firebase_Data/FirebaseManager.cs
@@ -4,104 +4,131 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity;
+using System.Linq;
 
 public class FirebaseManager : MonoBehaviour
 {
+    UserList userlist = new UserList();
+    UserList LeaderboardList = new UserList();
     DatabaseReference databaseReference;
     void Start()
     {
         databaseReference = FirebaseDatabase.DefaultInstance.RootReference;
-        for(int i = 1; i <=10; i++)
-        {
-            saveData(i + "00000000000000000xsdadhasdhads00000" + i, i * Random.Range(1000, 10000));
-        }
+        //for (int i = 1; i <= 10; i++)
+        //{
+        //    saveData(i + "00000000000000000xsdadhasdhads00000" + i, i * Random.Range(1000, 10000));
+        //}
+        PopulateLeaderBoard(5);
+        StartCoroutine(ShowLeaderboard());
     }
 
     void saveData(string id, int score)
     {
-        databaseReference.Child("Users").Child(id).Child("Score").SetValueAsync(score);
+        User user1 = new User();
+        user1.id = id;
+        user1.score = score.ToString();
+        userlist.Users.Add(user1);
+        var jsondata = JsonUtility.ToJson(userlist);
+        Debug.Log("Saved Data : " + jsondata);
+        databaseReference.SetRawJsonValueAsync(jsondata);
 
-        //databaseReference.Child("Users").Child(key).SetValueAsync(id).ContinueWith(task =>
+        /// Last Working Structure
+        //databaseReference.Child("Users").Child(id).Child("Score").SetValueAsync(score);
+
+    }
+
+
+    void PopulateLeaderBoard(int LoadLimit)
+    {
+        var top5 = FirebaseDatabase.DefaultInstance.GetReference("Users").OrderByChild("score").LimitToFirst(LoadLimit);
+        // Keep this query synced.
+        top5.KeepSynced(true);
+
+        top5.GetValueAsync().ContinueWith(task =>
+        {
+            if (task.Exception != null)
+            {
+                throw task.Exception;
+            }
+            if (!task.IsCompleted)
+            {
+                return;
+            }
+
+            // Iterate children in order based on "score"
+            if (task.Result != null)
+            {
+                foreach (var record in task.Result.Children)
+                {
+                    User user1 = new User();
+                    user1.id = record.Child("id").GetRawJsonValue();
+                    user1.score = record.Child("score").GetRawJsonValue();
+                    //Debug.Log(user1.id + " " + user1.score);
+                    LeaderboardList.Users.Add(user1);
+                    //Debug.Log("Loaded Data: ID: " + record.Child("id").GetRawJsonValue() + " Score: " + record.Child("score").GetRawJsonValue());
+                }
+
+            }
+        });
+
+        
+        ///Last Working Get UserList Data into Json!!!
+        //databaseReference.OrderByChild("score").GetValueAsync().ContinueWith(task =>
         //{
-        //    if(task.IsCompleted)
+        //    if (task.IsCompleted && !task.IsFaulted)
         //    {
-        //        databaseReference.Child("Users").Child(key).Child("Score").SetValueAsync(1000);
-        //    }
-        //    else
-        //    {
-        //        Debug.LogError("Error Creating a Databse Entry");
+        //        DataSnapshot data = task.Result;
+        //        var jsondata = data.GetRawJsonValue();
+        //        //UserList finalList = JsonUtility.FromJson<UserList>(jsondata);
+        //        UserList finalList = new UserList();
+        //        JsonUtility.FromJsonOverwrite(jsondata, finalList);
+        //        //Debug.Log("JSON: " + finalList.Users.Count);
+        //        finalList.Users.ForEach(x =>
+        //        {
+        //            Debug.Log("ID: " + x.id + "Score: " + x.score);
+        //        });
         //    }
         //});
-        //Dictionary<string, System.Object> entryValues = user1.ToDictionary();
-        //databaseReference.UpdateChildrenAsync(entryValues);
-        ///
-        /// Overwriting Code
-        ///
-        //User user1 = new User();
-        //user1.ID = id;
-        //string json = JsonUtility.ToJson(user1);
 
-        //databaseReference.Child("Users").SetRawJsonValueAsync(json).ContinueWith(task =>
+
+        //databaseReference.GetValueAsync().ContinueWith(task =>
         //{
-        //    if (task.IsCompleted)
+        //    if (task.IsCompleted && !task.IsFaulted)
         //    {
-        //        Debug.Log("Successfully added data to firebase");
-        //    }
-        //    else
-        //    {
-        //        Debug.Log("Databse Update not successful");
+        //        DataSnapshot data = task.Result;
+        //        var jsondata = data.GetRawJsonValue();
+        //        //UserList finalList = JsonUtility.FromJson<UserList>(jsondata);
+        //        UserList finalList = new UserList();
+        //        JsonUtility.FromJsonOverwrite(jsondata, finalList);
+        //        //Debug.Log("JSON: " + finalList.Users.Count);
+        //        finalList.Users.ForEach(x =>
+        //        {
+        //            Debug.Log("ID: " + x.id + "Score: " + x.score);
+        //        });
         //    }
         //});
     }
 
-    private void WriteNewScore(string userId, int score)
+    IEnumerator ShowLeaderboard()
     {
-        // Create new entry at /user-scores/$userid/$scoreid and at
-        // /leaderboard/$scoreid simultaneously
-        string key = databaseReference.Child("scores").Push().Key;
-        LeaderboardEntry entry = new LeaderboardEntry(userId, score);
-        Dictionary<string, System.Object> entryValues = entry.ToDictionary();
-
-        Dictionary<string, System.Object> childUpdates = new Dictionary<string, System.Object>();
-        childUpdates["/scores/" + key] = entryValues;
-        childUpdates["/user-scores/" + userId + "/" + key] = entryValues;
-
-        databaseReference.UpdateChildrenAsync(childUpdates);
+        yield return new WaitForSeconds(5f);
+        foreach (var child in LeaderboardList.Users)
+        {
+            Debug.Log(child.id + " " + child.score);
+        }
     }
 }
 
+[System.Serializable]
+public class UserList
+{
+    public List<User> Users = new List<User>();
+}
+
+[System.Serializable]
 public class User
 {
-    public string ID;
-
-    public Dictionary<string, System.Object> ToDictionary()
-    {
-        Dictionary<string, System.Object> result = new Dictionary<string, System.Object>();
-        result["id"] = ID;
-
-        return result;
-    }
-}
-
-public class LeaderboardEntry
-{
-    public string uid;
-    public int score = 0;
-
-
-    public LeaderboardEntry(string uid, int score)
-    {
-        this.uid = uid;
-        this.score = score;
-    }
-
-    public Dictionary<string, System.Object> ToDictionary()
-    {
-        Dictionary<string, System.Object> result = new Dictionary<string, System.Object>();
-        result["id"] = uid;
-        result["score"] = score;
-
-        return result;
-    }
+    public string id;
+    public string score;
 }
 
